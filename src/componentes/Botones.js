6010200417ce7cb2521321ede18158a8a76fb61e
Uciel22 <Ucielibarra@gmail.com>
2023-09-17 02:21:49 -0300

    agrego Botones.js actualizado

--- src/componentes/Botones.js
+++ src/componentes/Botones.js
@@ -15,7 +15,6 @@ function Botones(props){
     return(
         <div className="Botones">
            <div className="BotonesSuperiores BloqueBotones">
-<<<<<<< HEAD
                 <button name="piedra" className="Piedra Boton" onClick={clickImagen} disabled={props.estado.usuario == 3 || props.estado.pc == 3 ? true:false}>Piedra</button>
                 <button name="papel" className="Papel Boton" onClick={clickImagen} disabled={props.estado.usuario == 3 || props.estado.pc == 3 ? true:false}>Papel</button>
            </div>
@@ -24,16 +23,6 @@ function Botones(props){
                 <button className="Repetir Boton" onClick={clickReiniciar}>Repetir juego</button>
            </div>
            {props.estado.usuario == 3 || props.estado.pc == 3 ? <h2 className="Ganador">Resultado: {props.estado.usuario == 3 ? "🔥 Has ganado el juego 🔥": "😭 La PC gano el juego😭"} </h2> :false}
-=======
-                <button name="piedra" className="Piedra Boton" onClick={clickImagen} disabled={props.estado.usuario === 3 || props.estado.pc === 3 ? true:false}>Piedra</button>
-                <button name="papel" className="Papel Boton" onClick={clickImagen} disabled={props.estado.usuario === 3 || props.estado.pc === 3 ? true:false}>Papel</button>
-           </div>
-           <div className="BotonesInferiores BloqueBotones">
-                <button name="tijera" className="Tijera Boton" onClick={clickImagen} disabled={props.estado.usuario === 3 || props.estado.pc === 3 ? true:false}>Tijera</button>
-                <button className="Repetir Boton" onClick={clickReiniciar}>Repetir juego</button>
-           </div>
-           {props.estado.usuario === 3 || props.estado.pc === 3 ? <h2 className="Ganador">Resultado: {props.estado.usuario === 3 ? "🔥 Has ganado el juego 🔥": "😭 La PC gano el juego😭"} </h2> :false}
->>>>>>> a7ea714 (Agrego proyecto piedra papel tijeras en React)
         </div>
     )
       
